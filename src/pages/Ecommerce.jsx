abeb985dc87fed87afb239e1da008024ca7e5ac0
Omkar Londhe <imomkarlondhe@gmail.com>
2022-07-21 13:49:26 +0530

    update ecom

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+import { Stacked, Pie, Button, SparkLine } from "../components";
+import {
+  earningData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Ecommerce = () => {
-  return <div></div>;
+  const { currentColor } = useStateContext();
+  return (
+    <div className="mt-24">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="font-bold text-gray-400">Earnings</p>
+              <p className="text-2xl">$63,44878</p>
+            </div>
+            <button
+              type="button"
+              style={{ backgroundColor: currentColor }}
+              className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full p-4"
+            >
+              <BsCurrencyDollar />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Ecommerce;
